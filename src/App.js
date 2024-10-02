cf50b9c7477119584d28754ffec2c167ecc930a1
Christian Soldevilla <soldevilla.christian@gmail.com>
2024-10-02 12:43:25 -0500

    first component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+			<TodoItem/>
+			<TodoItem/>
+			<TodoItem/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,11 +18,21 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendamos React
         </a>
       </header>
     </div>
   );
+}
+
+function TodoItem(){
+	return(
+		<li>
+			<span>V</span>
+			<p>Llorar con la llorona</p>
+			<span>X</span>
+		</li>
+	);
 }
 
 export default App;
