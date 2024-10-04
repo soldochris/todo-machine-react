1269341e2793719bc8398cde1ae6698ed384f09e
Christian Soldevilla <soldevilla.christian@gmail.com>
2024-10-04 16:09:09 -0500

    counting TODOs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { TodoList } from './TodoList';
 import { CreateTodoButton } from './CreateTodoButton';
 import { TodoItem } from './TodoItem';
 import './App.css';
+import React from 'react';
 
 const defaultTodos = [
 	{text: 'Cut Onions', completed: false},
@@ -13,10 +14,19 @@ const defaultTodos = [
 ];
 
 function App() {
+	const [todos, setTodos] = React.useState(defaultTodos);
+	const [searchValue, setSearchValue] = React.useState("");
+
+	const completedTodos = todos.filter(todo => todo.completed).length;
+	const totalTodos = todos.length;
+
   return (
     <>
-			<TodoCounter completed={3} total={5} />
-			<TodoSearch/>
+			<TodoCounter completed={completedTodos} total={totalTodos} />
+			<TodoSearch
+				searchValue = {searchValue}
+				setSearchValue = {setSearchValue}
+			/>
 			<TodoList>
 				{defaultTodos.map(todo => (
 					<TodoItem 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch(){
+function TodoSearch({searchValue, setSearchValue}){
 	
-	const [searchValue, setSearchValue] = React.useState("");
-	console.log(searchValue);
-
 	return(
 		<input 
 			type="text"
